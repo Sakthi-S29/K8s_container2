2a90f0d6021df70edd9198d2ec3e2d7f96697230
Sakthi Sharan Mahadevan <sakthisharan@Sakthis-MacBook-Pro.local>
2025-03-22 07:18:59 -0300

    terraform update

--- TF/gke.tf
+++ TF/gke.tf
@@ -3,35 +3,32 @@ provider "google" {
   region  = "us-central1"
 }
 
-resource "google_container_cluster" "gke_cluster"{
-    name    = "k8s-cluster"
-    location = "us-central1-c"
-    remove_default_node_pool = true
-    initial_node_count = 1
-    deletion_protection = false
-
+resource "google_container_cluster" "gke_cluster" {
+  name               = "k8s-cluster"
+  location           = "us-central1-c"
+  remove_default_node_pool = true
+  deletion_protection = false
 }
 
 resource "google_container_node_pool" "default_pool" {
-    name = "default-pool"
-    location = "us-central1-c"
-    node_count = 1
-    cluster = "k8s-cluster"
+  name       = "default-pool"
+  location   = "us-central1-c"
+  cluster    = google_container_cluster.gke_cluster.name
+  node_count = 1
 
-    node_config {
-      machine_type = "e2-medium"
-      disk_size_gb = 10
-      disk_type = "pd-standard"
-      image_type = "cos_containerd"
-    }
+  node_config {
+    machine_type = "e2-medium"
+    disk_size_gb = 10
+    disk_type    = "pd-standard"
+    image_type   = "cos_containerd"
+  }
+
+  depends_on = [google_container_cluster.gke_cluster]
 }
 
-
-
 resource "google_compute_disk" "shared-disk" {
-  name ="shared-disk"
+  name = "shared-disk"
   type = "pd-standard"
   zone = "us-central1-c"
   size = 1
 }
-
