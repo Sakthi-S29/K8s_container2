de5ab4a8c39c32903f659e63f61f0be2edc7e3da
Sakthi Sharan Mahadevan <sakthisharan@Sakthis-MacBook-Pro.local>
2025-03-22 13:00:58 -0300

    testing

--- TF/gke.tf
+++ TF/gke.tf
@@ -7,7 +7,7 @@ provider "google" {
 resource "google_container_cluster" "primary" {
   name     = "k8s-cluster"
   location = "us-central1-c"  # Zone for the GKE cluster
-
+  deletion_protection = false
   remove_default_node_pool = true
   initial_node_count       = 1
 
